863ebfb1911c28f6180fc9d8acb58dfcc73be2d1
Erick Fabricio Martínez Castellanos <mail@erickfabricio.com>
2019-11-01 11:47:39 -0600

    Callback funcionalidad

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Reactive - RxJs");
+/**
+ * Callback: Es una función que se le pasa como parametro otra función,
+ * se utilíza para tratar eventos síncronos.
+ * 
+ * 
+ *  
+ **/
+
+
+
+// ********* #1 - Procesar un array - callback ********* //
+var zfighters = ['Goku', 'Vegeta', 'Gohan', 'Piccolo'];
+
+zfighters.forEach(function (name, index) {
+    console.log(index + 1 + '-' + name);
+});
+/**
+ * Print
+ * 1-Goku
+ * 2-Vegeta
+ * 3-Gohan
+ * 4-Piccolo
+ */
+
+ // ********* #2 - Procesar un array ********* //
